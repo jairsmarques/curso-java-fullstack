d2c277209887a4eec62cee80ad5376001506cbda
Jair Marques <jairsmarques1126@gmail.com>
2024-11-25 17:53:39 -0300

    adicionando exemplo de calculo con números inteiros

---
+++ primeiro-programa-java/src/cursojava/executavel/OperacoesEntreNumerosInteiros.java
@@ -0,0 +1,16 @@
+package cursojava.executavel;
+
+public class OperacoesEntreNumerosInteiros {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Medias da notas = "  + (nota1 + nota2 + nota3 + nota4) / 4);
+
+	}
+
+}
