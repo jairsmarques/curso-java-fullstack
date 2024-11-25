06eea97e256f759e6bdf7acc82ad283395a4d51a
Jair Marques <jairsmarques1126@gmail.com>
2024-11-25 17:57:39 -0300

    exemplo de calculo com variáveis do tipo double

---
+++ primeiro-programa-java/src/cursojava/executavel/OperacoesEntreNumerosFlutuantes.java
@@ -0,0 +1,16 @@
+package cursojava.executavel;
+
+public class OperacoesEntreNumerosFlutuantes {
+
+	public static void main(String[] args) {
+		
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		
+		System.out.println("Medias da notas = "  + (nota1 + nota2 + nota3 + nota4) / 4);
+
+	}
+
+}
